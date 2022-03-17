f076a9ba8e2f19d383321cc092e39a2cb24c70c1
unknown <tkdgns25300@gmail.com>
2022-03-17 18:02:55 +0900

    추가: 서버-데이터베이스 연결(#16)

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require("cookie-parser");
+const connectDB = require('./db/connect');
+const notFound = require('./middleware/notFound');
 const app = express();
 const PORT = process.env.PORT || 80;
 
@@ -16,9 +18,19 @@ app.use(cookieParser());
 app.use('/', (req, res) => {
     res.send("PIC. Project")
 })
+app.use(notFound)
 
 
 // start server
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}...`);
-});
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, () => {
+            console.log(`Server is listening on port ${PORT}...`);
+        });
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+start();
