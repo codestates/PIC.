1d8d74d8f91dd11361b174299ec20e1d45d87eb2
hun <tkdgns25300@gmail.com>
2022-03-18 15:40:25 +0900

    추가: send email API 작성(#40)

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,14 +1,69 @@
+require('dotenv').config();
 const User = require('../models/User');
 const asyncWrapper = require('../middleware/async');
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const path = require('path');
+var appDir = path.dirname(require.main.filename);
 
 
-//
-const temp = asyncWrapper(async (req, res) => {
-    res.send('controller ok');
+/********** Auth Number **********/
+let authNum;
+
+
+// 이메일 인증
+const sendMail = asyncWrapper(async (req, res) => {
+    const { email } = req.body;
+    if (!email) {
+        res.json({
+            message: "require email"
+        })
+    } else {
+        authNum = String(Math.random()).split('').slice(2, 8).join('');
+        let emailTemplate;
+        ejs.renderFile(appDir + '/template/authMail.ejs', { authCode: authNum }, function (err, data) {
+            if (err) throw err;
+            emailTemplate = data;
+        })
+
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 587,
+            auth: {
+                user: process.env.NODEMAILER_USER,
+                pass: process.env.NODEMAILER_PASS
+            }
+        });
+
+        let mailOptions = {
+            from: 'PIC.',
+            to: email,
+            subject: '회원가입을 위한 인증번호를 입력해주세요.',
+            html: emailTemplate
+        };
+
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) throw err;
+            console.log(authNum);
+            res.json({
+                authNum: authNum,
+                message: "success"
+            })
+            transporter.close();
+        })
+    }
+})
+
+
+// 회원 가입
+const signup = asyncWrapper(async (req, res) => {
+
 })
 
 
 
 module.exports = {
-    temp
+    sendMail,
+    signup,
 }
--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -4,6 +4,7 @@ const router = express.Router();
 // middleware
 const controllers = require('../controllers/authController');
 
-router.get('/', controllers.temp)
+router.post('/mail', controllers.sendMail);
+router.get('/signup', controllers.signup);
 
 module.exports = router;
