83e46ed4819c15b28850cc8c932733b29cc9cea1
hun <tkdgns25300@gmail.com>
2022-03-18 11:25:55 +0900

    Post스키마 추가(#17)

---
+++ server/models/Post.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema(
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: [true, "must provide title"],
+    },
+    description: {
+      type: String,
+      trim: true,
+      default: "",
+    },
+    photo: {
+      type: String,
+      required: [true, "must provide photo"],
+    },
+    location: {
+      type: mongoose.Schema.Types.Mixed,
+      required: [true, "must provide location"],
+    },
+    hashtags: {
+      type: Array,
+      default: [],
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    comment: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Post", PostSchema);
