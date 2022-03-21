5c0be93e871b7ba181e532b8344e77e43e312975
hun <tkdgns25300@gmail.com>
2022-03-21 12:03:32 +0900

    추가: upload a post API작성(#34)

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,1 +1,23 @@
-// test
+const Post = require('../models/Post');
+const asyncWrapper = require('../middleware/async');
+const verifyToken = require('../utils/verifyToken');
+
+
+// 게시글 업로드
+const uploadPost = asyncWrapper(async (req, res) => {
+    const { title, photo, location } = req.body;
+    if (title && photo && location) {
+        const data = verifyToken(req.headers.authorization, 'accessToken');
+        req.body.author = data.id;
+        await Post.create(req.body);
+        res.status(201).json({ message: "success" });
+    } else {
+        res.status(400).json({ message: "fail : all required fields are not filled in" });
+    }
+})
+
+
+
+module.exports = {
+    uploadPost
+}
