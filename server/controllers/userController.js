0d0b540ecdf421925739a6a70527267742455a31
hun <tkdgns25300@gmail.com>
2022-03-21 10:55:57 +0900

    추가: update user information API작성(#32)

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -16,6 +16,27 @@ const getUserInfo = asyncWrapper(async (req, res) => {
 })
 
 
+// 사용자 정보 업데이트
+const updateUserInfo = asyncWrapper(async (req, res) => {
+    if (Object.keys(req.body).length === 0) { // nickname, password, image 셋 다 request에 없을 경우
+        res.status(400).json({ message: "fail : require nickname or password or image" });
+    } else {
+        const userInfo = await User.findById(req.params.id);
+        if (!userInfo) {
+            res.status(400).json({ message: "fail : invalid user id" });
+        } else {
+            await User.updateOne({ _id: req.params.id }, req.body, {
+                runValidators: true
+            });
+            res.status(200).json({ message: "success" });
+        }
+    }
+})
+
+
+
+
 module.exports = {
-    getUserInfo
+    getUserInfo,
+    updateUserInfo
 }
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 // middleware
+const checkToken = require('../middleware/checkToken');
 const controllers = require('../controllers/userController');
 
 router.get('/:id', controllers.getUserInfo);
+router.patch('/:id', checkToken, controllers.updateUserInfo);
 
 
 module.exports = router;
