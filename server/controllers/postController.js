39b2bfb708388243191a02634734f2261efd12af
hun <tkdgns25300@gmail.com>
2022-03-29 13:49:31 +0900

    수정: 거리에 따른 게시글 조회 API수정(#97)(미완)

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -2,6 +2,7 @@ const Post = require('../models/Post');
 const User = require('../models/User');
 const asyncWrapper = require('../middleware/async');
 const verifyToken = require('../utils/verifyToken');
+const isSubsetOf = require('../utils/isSubsetOf');
 
 
 // 게시글 업로드
@@ -54,27 +55,6 @@ const getAllPosts = asyncWrapper(async (req, res) => {
             message: "success"
         })
     }
-
-    // 부분집합 여부 확인하는 함수
-    function isSubsetOf(base, sample) {
-        base.sort();
-        sample.sort();
-        const findItemInSortedArr = (item, arr, from) => {
-            for (let i = from; i < arr.length; i++) {
-                if (item === arr[i]) return i;
-            }
-            return -1;
-        };
-
-        let baseIdx = 0;
-        for (let i = 0; i < sample.length; i++) {
-            baseIdx = findItemInSortedArr(sample[i], base, baseIdx);
-            if (baseIdx === -1) {
-                return false;
-            }
-        }
-        return true;
-    };
 })
 
 
